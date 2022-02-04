ed46168e3864983d16c6f43a50c5daba65abfad9
akbarob <96150105+akbarob@users.noreply.github.com>
2022-02-04 01:23:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
+function load(){
+    var q = prompt("What do you like to be called?")
+   
+document.getElementById("name").innerHTML = "hello" +" "+ q ; 
+}
+
+
 
 function compute(){
-    var years = document.getElementById("years").value;
     var amount = document.getElementById("principal").value;
-    
-    
     var rate = document.getElementById("rate").value;
-    var d = new Date() ;
+    
+    if (amount == "" || amount <= 0){
+        alert("Please fill the amount field.");
+        document.getElementById("principal").focus();
+        return;
+    }
+    else if (rate =="0"|| parseInt(rate) <= 0)
+     {
+        alert("Please fill out the rate field with a number higher than zero.");
+        document.getElementById("rate").focus();
+        return;
+    }
+    var years = document.getElementById("years").value;
+     var d = new Date() ;
     v = parseInt(d.getFullYear()) + parseInt(years);
     result = (years*amount*rate)/100;
     newAmount = parseInt(amount) + parseInt(result);
     console.log(newAmount);
     
-
-    if (document.getElementById("bb").onclick) 
-    
-    if (amount == ""){
-        alert("Please fill the amount field.");
-        document.getElementById("amount").focus();
-        return false;
-    } else if (parseInt(amount) <= 0) {
-        alert("Please fill out the amount field with a number higher than zero.");
-        document.getElementById("amount").focus();
-        return false;
-    } else if (parseInt(rate) == 0) {
-        alert("Please fill out the rate field with a number higher than zero.");
-        document.getElementById("rate").focus();
-        return false;
-    }
-    
-{
-    document.getElementById("akbar").innerHTML = "If you deposit" + " " + amount + "," + "<br>" +"at an interest rate of" +" "+ rate +"%."+"<br>"+ "You will recieve an amount of" +" "+newAmount+ "," +"<br>" + "in the year" +" "+ v + "."
+    {
+    document.getElementById("akbar").innerHTML = "If you deposit" + " " + "<span class='yellow'>" + amount + "</span>" + "," + "<br>" + "at an interest rate of" + " " + "<span class='yellow'>" + rate +"%." + "</span>"+"<br>" + "You will recieve an amount of" + " " + "<span class='yellow'>"+newAmount+ "</span>" + "," + "<br>" + "in the year" + " " + "<span class='yellow'>"+ v + "</span>" +"."
    
     
 }
